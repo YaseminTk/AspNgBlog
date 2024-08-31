6d2ed45c1f01292ef13688c5597d38a57653c033
hakanttek <hakanttek@gmail.com>
2024-08-31 02:47:13 +0200

    feat: Update navbar with login/logout functionality and Material components
    - Refactored navbar to use Material menu and button components for better UI/UX.
    - Added conditional rendering to display login form or logout menu based on authentication status.
    - Integrated Material Menu to handle logout actions more effectively.

--- src/ng-blog/src/app/components/navbar/navbar.component.ts
+++ src/ng-blog/src/app/components/navbar/navbar.component.ts
@@ -3,11 +3,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { LoginComponent } from '../login/login.component'
 import { AuthService } from '../../services/auth.service'
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'navbar',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
@@ -20,7 +22,7 @@ export class NavbarComponent {
   }
 
   async toggleAuth() {
-    if(this.authService.isLogedIn)
+    if (this.authService.isLogedIn)
       await this.authService.logoutAsync();
     else
       this.dialog.open(LoginComponent, this.dialogConfig);
